68fe339b95ed26f76ee6fa5a35948d68e8760e03
Ulrick <serveauxulrick@gmail.com>
2026-09-10 15:46:31 +0400

    Limit source directory to covered news

--- src/Repository/SourceRepository.php
+++ src/Repository/SourceRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Source;
+use App\InfoTrak\Catalog;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -17,7 +18,10 @@ class SourceRepository extends ServiceEntityRepository
     /** @return Source[] */
     public function findWithWebsite(): array
     {
-        return $this->createQueryBuilder('s')->where('s.websiteUrl IS NOT NULL')
-            ->andWhere("s.websiteUrl != ''")->orderBy('s.name', 'ASC')->getQuery()->getResult();
+        return $this->createQueryBuilder('s')->distinct()->innerJoin('s.articles', 'a')
+            ->where('s.websiteUrl IS NOT NULL')->andWhere("s.websiteUrl != ''")
+            ->andWhere('a.isDemo = false')->andWhere('a.place IN (:coveredZones)')
+            ->setParameter('coveredZones', Catalog::ZONES)
+            ->orderBy('s.name', 'ASC')->getQuery()->getResult();
     }
 }
